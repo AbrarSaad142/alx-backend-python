fa7c59d05cf47429c0746c82274d11d05987844f
Abrar Saad <abrarsaad3572022@gmail.com>
2024-08-02 02:27:05 +0300

    return a sum of all nums inside a list

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 """function sum_mixed_list decleration"""
 
 
-from typing import List, Union
+from typing import Tuple, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int,float]]) -> float:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """return a sum of all nums inside a list"""
-    return sum(mxd_lst)
+    return (k, v**2)
