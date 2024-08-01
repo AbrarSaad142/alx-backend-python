fa9b7a301294017819ab29e0f26cd44d91638c72
Abrar Saad <abrarsaad3572022@gmail.com>
2024-08-02 02:30:13 +0300

    return a sum of all nums inside a list

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,9 +2,6 @@
 """function sum_mixed_list decleration"""
 
 
-from typing import Tuple, Union
-
-
 def make_multiplier(multiplier: float) -> callable[[float], float]:
     """return a sum of all nums inside a list"""
     def multiplies(n: float):
