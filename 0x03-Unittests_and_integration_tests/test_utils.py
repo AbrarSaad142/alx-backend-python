d26dd78ef4d722869de4b38ef1f1ebe9257d8980
Abrar Saad <abrarsaad3572022@gmail.com>
2024-08-27 02:42:20 +0300

    0. UTF-8 Validation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,7 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("apip install parameterized",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(
